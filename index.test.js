a052e6dbd11b894e01feb1215dce14f06cc878b7
BigBearWayne <93151862+BigBearWayne@users.noreply.github.com>
2022-03-29 16:12:08 -0700

    added action inputs

--- index.test.js
+++ index.test.js
@@ -4,8 +4,11 @@ const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
+  process.env['INPUT_PROJECT_NAME'] = 'test-project-name';
+  process.env['INPUT_AWS_ID'] = 'AWS_ID';
+  process.env['INPUT_AWS_SECRET'] = 'AWS_SECRET';
+  process.env['INPUT_DEPLOYMENT_FILE'] = 'deployment-test.json'
   const ip = path.join(__dirname, 'index.js');
-  const result = cp.exec(`node ${ip}`, {env: process.env}).toString();
+  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
   console.log(result);
 })
